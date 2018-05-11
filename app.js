cd1e04c66f0b39fdf57c723b7a12bb0e35cf5184
Pawel <pawel.klasa@gmail.com>
2018-05-11 22:29:23 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // hello
+let container = document.getElementById('container');
+let menuButton = document.getElementById('menu-button');
+let menu = document.getElementById('menu');
+
+menuButton.addEventListener('click', function() {
+  console.log('clicked');
+  menu.style.width = '300px';
+  container.setAttribute('style', 'grid-auto-columns: 300px 1fr 1fr;');
+});
