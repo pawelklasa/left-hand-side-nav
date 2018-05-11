5d50933aec07c59f81b186123c50e0e672145b09
Pawel <pawel.klasa@gmail.com>
2018-05-11 22:43:09 +0100

    opening a menu

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 let container = document.getElementById('container');
 let menuButton = document.getElementById('menu-button');
 let menu = document.getElementById('menu');
+let close = document.getElementById('close');
 
 menuButton.addEventListener('click', function() {
-  console.log('clicked');
-  menu.style.width = '300px';
-  container.setAttribute('style', 'grid-auto-columns: 300px 1fr 1fr;');
+  menu.style.width = '200px';
+  container.setAttribute('style', 'grid-auto-columns: 200px 1fr 1fr;');
+  close.innerHTML = 'close';
 });
